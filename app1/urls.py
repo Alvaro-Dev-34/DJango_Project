e9ef304dac1fce9597974973ec8ea809a516973d
Alvaro-Dev-34 <alvaro.rivassolera@gmail.com>
2024-12-16 20:26:20 +0100

    creo formularios y conecto las paginas a traves de un menu

--- app1/urls.py
+++ app1/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
-    path('hello/<str:username>', views.hello),
+    path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('hello/<str:username>', views.hello, name="hello"),
 
-    path('projects/', views.projects),
-    path('tasks/', views.tasks)
+    path('projects/', views.projects, name="projects"),
+    path('projects/<int:id>', views.project_detail, name="project_detail"),
+    path('create_project/', views.create_project, name="create_project"),
+
+    path('tasks/', views.tasks, name="tasks"),
+    path('create_task/', views.create_task, name="create_task")
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CreateNewTask, CreateNewProject
 
 
 # Create your views here.
@@ -25,18 +26,54 @@ def about(request):
 
 
 def projects(request):
-    #projects = list(Project.objects.values())
+    # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html', {
+    return render(request, 'projects/projects.html', {
         'projects': projects
     })
     # return render(projects_all, safe=False)
 
 
 def tasks(request):
-    #task = get_object_or_404(Task, id=id)
+    # task = get_object_or_404(Task, id=id)
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {
+    return render(request, 'tasks/tasks.html', {
         'tasks': tasks
     })
     # return render('task: %s' % task.title)
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(
+            title=request.POST['title'],
+            description=request.POST['description'],
+            projects_id=2)
+        return redirect(
+            'tasks')  # esta redireccion  es usando el nombre que le he dado a la url (como si fuera una variable)
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {
+            'form': CreateNewProject()
+        })
+    else:
+        print(request.POST)
+        project = Project.objects.create(name=request.POST["name"])
+        print(project)
+        return redirect('projects')
+
+
+def project_detail(request, id):
+    print(id)
+    project = get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(projects_id=id)
+    return render(request, 'projects/detail.html', {
+        'project': project,
+        'tasks': tasks
+    })
